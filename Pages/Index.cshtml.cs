16a7814bc68753685c74b492cbd439dd48ce634e
BrennanConroy <brecon@microsoft.com>
2019-01-12 13:50:44 -0800

    Fix app name and display stream name correctly

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace WebApplication1.Pages
+namespace StreamR.Pages
 {
     public class IndexModel : PageModel
     {
