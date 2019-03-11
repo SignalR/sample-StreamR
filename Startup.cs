ded6a1da64d95995ff08f1f6f08f289ad287996c
David Fowler <davidfowl@gmail.com>
2019-03-10 22:19:26 -0700

    Minor sample cleanup  (#1)
    - Put things in namespaces
    - Use 3.0 template
    - Use IAsyncEnumerable

--- Startup.cs
+++ Startup.cs
@@ -1,14 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace StreamR
 {
@@ -30,22 +26,17 @@ namespace StreamR
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
-
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
 
-            services.AddSignalR(o =>
-            {
-                o.EnableDetailedErrors = true;
-            });
+            services.AddSignalR();
 
             services.AddSingleton<StreamManager>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment environment)
         {
-            if (env.IsDevelopment())
+            if (environment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
--- StreamManager.cs
+++ StreamManager.cs
@@ -5,41 +5,36 @@ using System.Threading.Channels;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 
-public class StreamManager
+namespace StreamR
 {
-    private readonly ConcurrentDictionary<string, StreamHolder> _streams;
-    private long _globalClientId;
-
-    public StreamManager()
+    public class StreamManager
     {
-        _streams = new ConcurrentDictionary<string, StreamHolder>();
-    }
+        private readonly ConcurrentDictionary<string, StreamHolder> _streams = new ConcurrentDictionary<string, StreamHolder>();
+        private long _globalClientId;
 
-    public List<string> ListStreams()
-    {
-        var streamList = new List<string>();
-        foreach (var item in _streams)
+        public List<string> ListStreams()
         {
-            streamList.Add(item.Key);
-        }
-        return streamList;
-    }
-
-    public async Task RunStreamAsync(string streamName, ChannelReader<string> stream)
-    {
-        var streamHolder = new StreamHolder() { Source = stream };
-
-        // Add before yielding
-        // This fixes a race where we tell clients a new stream arrives before adding the stream
-        _streams.TryAdd(streamName, streamHolder);
-
-        await Task.Yield();
-
-        try
-        {
-            while (await stream.WaitToReadAsync())
+            var streamList = new List<string>();
+            foreach (var item in _streams)
             {
-                while (stream.TryRead(out var item))
+                streamList.Add(item.Key);
+            }
+            return streamList;
+        }
+
+        public async Task RunStreamAsync(string streamName, ChannelReader<string> stream)
+        {
+            var streamHolder = new StreamHolder() { Source = stream };
+
+            // Add before yielding
+            // This fixes a race where we tell clients a new stream arrives before adding the stream
+            _streams.TryAdd(streamName, streamHolder);
+
+            await Task.Yield();
+
+            try
+            {
+                await foreach (var item in stream.ReadAllAsync())
                 {
                     foreach (var viewer in streamHolder.Viewers)
                     {
@@ -51,49 +46,50 @@ public class StreamManager
                     }
                 }
             }
-        }
-        finally
-        {
-            RemoveStream(streamName);
-        }
-    }
-
-    public void RemoveStream(string streamName)
-    {
-        _streams.TryRemove(streamName, out var streamHolder);
-        foreach (var viewer in streamHolder.Viewers)
-        {
-            viewer.Value.Writer.TryComplete();
-        }
-    }
-
-    public ChannelReader<string> Subscribe(string streamName, CancellationToken token)
-    {
-        if (!_streams.TryGetValue(streamName, out var source))
-        {
-            throw new HubException("stream doesn't exist");
+            finally
+            {
+                RemoveStream(streamName);
+            }
         }
 
-        var id = Interlocked.Increment(ref _globalClientId);
-
-        var channel = Channel.CreateBounded<string>(options: new BoundedChannelOptions(2) {
-            FullMode = BoundedChannelFullMode.DropOldest
-        });
-
-        source.Viewers.TryAdd(id, channel);
-
-        // Register for client closing stream, this token will always fire (handled by SignalR)
-        token.Register(() =>
+        public void RemoveStream(string streamName)
         {
-            source.Viewers.TryRemove(id, out _);
-        });
+            _streams.TryRemove(streamName, out var streamHolder);
+            foreach (var viewer in streamHolder.Viewers)
+            {
+                viewer.Value.Writer.TryComplete();
+            }
+        }
 
-        return channel.Reader;
-    }
+        public IAsyncEnumerable<string> Subscribe(string streamName, CancellationToken cancellationToken)
+        {
+            if (!_streams.TryGetValue(streamName, out var source))
+            {
+                throw new HubException("stream doesn't exist");
+            }
 
-    private class StreamHolder
-    {
-        public ChannelReader<string> Source;
-        public ConcurrentDictionary<long, Channel<string>> Viewers = new ConcurrentDictionary<long, Channel<string>>();
+            var id = Interlocked.Increment(ref _globalClientId);
+
+            var channel = Channel.CreateBounded<string>(options: new BoundedChannelOptions(2)
+            {
+                FullMode = BoundedChannelFullMode.DropOldest
+            });
+
+            source.Viewers.TryAdd(id, channel);
+
+            // Register for client closing stream, this token will always fire (handled by SignalR)
+            cancellationToken.Register(() =>
+            {
+                source.Viewers.TryRemove(id, out _);
+            });
+
+            return channel.Reader.ReadAllAsync();
+        }
+
+        private class StreamHolder
+        {
+            public ChannelReader<string> Source;
+            public ConcurrentDictionary<long, Channel<string>> Viewers = new ConcurrentDictionary<long, Channel<string>>();
+        }
     }
 }
